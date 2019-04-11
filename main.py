5a554545a3d90d758d9df9cd82f6c36d52c1dcad
Root31 <sebastiaandevries1998@outlook.com>
2019-04-12 01:21:58 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+from __future__ import print_function
+import pyzbar.pyzbar as pyzbar
+import numpy as np
+import cv2
+import os
+
+def decode(im):
+
+    # zoek QR code
+    decodedObjects = pyzbar.decode(im)
+
+    # print resultaat
+    return decodedObjects
+
+def waardepakken():
+    while True:
+        os.system("raspistill -t 750 -o /home/pi/Documents/cameraFiles/image.png") #laat camera een foto maken
+        im = cv2.imread('/home/pi/Documents/cameraFiles/image.png')
+        decodedObjects = decode(im)
+        if len(decodedObjects)>0:
+            tijdelijk = str(decodedObjects[0][0]) #relevante data van bytewaarde naar string
+            #print(tijdelijk)
+            #waarde = int((tijdelijk[0])) # string naar integer # Alleen voor python3 KUT!
+            if tijdelijk > 0:
+                return tijdelijk
+
+# Main
+if __name__ == '__main__':
+    print(waardepakken())
